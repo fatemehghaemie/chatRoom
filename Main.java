ffcd92d12aad73de80d1b5e14fd07ce1d0ef7122
fatemehghaemie <161334564+fatemehghaemie@users.noreply.github.com>
2024-06-14 20:24:37 +0330

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,69 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.net.Socket;
-import java.util.List;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.io.IOException;
 
-
 public class Main {
-    private static int clientCounter = 1;
-    private static List<Client> connectedClients = new ArrayList<>();
 
     public static void main(String[] args) {
+
         try {
-            ServerSocket serverSocket = new ServerSocket(1234);
-            System.out.println("Chat Server is running on port 1234");
+            Socket socket = new Socket("localhost", 1234);// کلاینت
+            Scanner input = new Scanner(socket.getInputStream());
+
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner userInput = new Scanner(System.in);
+
+            Thread messageListener = new Thread(() -> {
+                try {
+                    while (true) {
+                        String chatMessage = input.nextLine();
+                        System.out.println(chatMessage);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            messageListener.start();
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                Client client = new Client(clientSocket,clientCounter);
-                connectedClients.add(client);
-                clientCounter++;
-                client.start();
+                String chatMessage = userInput.nextLine();
+                output.println(chatMessage);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    static class Client extends Thread {
-        private int clientId;
-        private PrintWriter output;
-        private Socket clientSocket;
-
-
-        public Client(Socket socket, int clientId) {
-            this.clientSocket = socket;
-            this.clientId = clientId;
-        }
-
-        public void run() {
-            try {
-                Scanner input = new Scanner(clientSocket.getInputStream());
-
-                output = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                output.println("Connected to chat server");
-
-                while (true) {
-                    if (input.hasNextLine()) {
-                        String chatMessage = input.nextLine();
-                        System.out.println("User" +clientId+ ": " +chatMessage);
-                        broadcastMessage("User" +clientId+ ": " + chatMessage);
-                    }
-                }
-            } catch (IOException e) {
-
-                e.printStackTrace();
-            }
-        }
-
-        private void broadcastMessage(String message) {
-            for (Client handler :connectedClients) {
-                handler.output.println(message);
-            }
         }
     }
 }
